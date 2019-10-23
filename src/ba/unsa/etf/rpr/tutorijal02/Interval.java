3967fe8713ecc4f97f9d43ba8d6a24ed3185eee0
hhaskic1 <hhaskic1@etf.unsa.ba>
2019-10-23 16:46:36 +0200

    Zapocet zadatak 2

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -3,4 +3,21 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Interval {
 
 
+    public Interval(double v, double v1, boolean b, boolean b1) {
+    }
+
+    public boolean isIn(double v) {
+        boolean a = false;
+        return a;
+    }
+
+    public boolean isNull() {
+        boolean b = false;
+        return b;
+    }
+
+    public Interval intersect(Interval interval) {
+        Interval a;
+        return interval;
+    }
 }
